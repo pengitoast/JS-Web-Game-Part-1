8e11e48eea846c1b880b55b5d4585571b63b3867
Meng Po <pengitoast@gmail.com>
2022-06-21 09:20:24 -0600

    yes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -13,6 +20,12 @@ function newImage(url, left, bottom){
         object.remove()
     })
  }
+ let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 
 
